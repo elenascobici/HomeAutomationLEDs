235255b00a5261a4278754450c210d2ef45019d3
dinus <dinus@hotmail.com>
2022-12-23 14:59:00 -0500

    Suggested changes to avoid memory leaks

--- LEDLightsAtmega/LEDLightsAtmega.ino
+++ LEDLightsAtmega/LEDLightsAtmega.ino
@@ -5,7 +5,6 @@
 #include <ColoursFade.h>
 
 const int data_pin = 5;
-//const int clock_pin = 6;
 const int ledsCount = 495;
 char goodReply[] = "Atmega Ok!";
 char badReply[] = "Atmega Error";
@@ -13,29 +12,24 @@ const bool debugMode = false;
 
 CRGB leds[ledsCount];
 
-// Requests are in the form of c1.255.255.0.c2.0.15.6.p31.d10.b50
+// Requests are in the form of [COLOR1][COLOR1 RGB VALUE][COLOR2][COLOR2 RGB VALUE]...[COLORn][COLORn RGB VALUE][PRESET NUMBER][DELAY VALUE][BRIGHTNESS VALUE]
+// Notes: RGB value should be in dotted decimal format. Delay value should be specified in milliseconds. Brightness value should be in percentage format (1 ... 100%).
+// Example: c1.255.255.0.c2.0.15.6.p31.d10.b50
 
-char oneByte;
+char oneChar;
 String packetString = "";
 
 int patternDelayBrightness [3];
 
-String currentCommandString;
 Pattern* currentPattern;
-
-//PatternParams* genericPatternParams = new PatternParams();
-FillSolid* fillSolidObj; 
-ColoursFade* coloursFadeObj;
 
 void setup() {
   Serial1.begin(9600);
   FastLED.addLeds<WS2812, data_pin, RGB>(leds, ledsCount);
-
   pinMode(data_pin, OUTPUT);
 }
 
-void showMessage(String message)
-{
+void showMessage(String message){
   if (debugMode)
   {
     Serial1.print(message + "\n\r");
@@ -43,41 +37,37 @@ void showMessage(String message)
 }
 
 void loop() {
-  bool finished = false;
-  //showMessage("I'm in the loop");
+  bool fullCommandReceived = false;
+  showMessage("I'm in the loop");
   if (Serial1.available() > 0) {
-    oneByte = Serial1.read();
-    //showMessage("I got: " + String(oneByte));
-    if (oneByte != '<' && packetString == ""){
-      //showMessage(badReply);
+    oneChar = Serial1.read();
+    showMessage("I got: " + String(oneChar));
+    if (oneChar != '<' && packetString == ""){
+      showMessage(badReply);
     }
     else{
-      packetString += oneByte;
+      packetString += oneChar;
     }
-    if (oneByte == '>') {
-      //showMessage(String(goodReply) + " packets: " + String(packetString));
+    if (oneChar == '>') {
+      showMessage(String(goodReply) + " packets: " + String(packetString));
       packetString = packetString.substring(1, packetString.length() - 1);
-      finished = true;
+      fullCommandReceived = true;
     }
   }
-  String commandString = packetString;
-  //showMessage("Command string: " + commandString);
-  if (commandString != "" && finished){
-    finished = false;
-    //showMessage("Received command string: " + commandString);
-    currentCommandString = commandString;
+  showMessage("Command string: " + packetString);
+  if (packetString != "" && fullCommandReceived){
+    showMessage("Received command string: " + packetString);
+    fullCommandReceived = false;
+    delete currentPattern;
+    currentPattern = CreatePattern(packetString);
     packetString = "";
-    currentPattern = CreatePattern(currentCommandString);
     currentPattern->Start();
-//    FastLED.setBrightness(100);
-//    fill_solid(leds, ledsCount, CRGB(255, 0, 0));
-//    FastLED.show();
-    //showMessage("Received new command");
-  }
-  else if (currentPattern != NULL){
+  } else if (currentPattern != NULL){
     bool yeah = currentPattern->NextStep();
     showMessage(String(yeah));
-    //showMessage("Command is same as previous");
+    showMessage("Continuie executing existing pattern");
+  } else {
+    showMessage("Unexpected command received");
   }
 }
 
@@ -86,12 +76,9 @@ String CutPeriod(String command){
   return command.substring(command.indexOf('.') + 1, command.length() - command.indexOf('.') + 3);
 }
 
-PatternParams* patternParams;
-
 // Create an instance of the PatternParams structure, set the proper attributes and return it
 PatternParams* SetPatternAttributes(String commandString){
-  delete patternParams;
-  patternParams = new PatternParams();
+  PatternParams* patternParams = new PatternParams();
   int i = 0;
   // While the first character of packetString is c, there are more colours to add
   while (commandString[0] == 'c') {
@@ -126,13 +113,11 @@ PatternParams* SetPatternAttributes(String commandString){
   showMessage("Right before leds Count and leds: " + commandString);
   patternParams->ledsCount = ledsCount;
   patternParams->leds = leds;
-  //patternParams->pin = pin;
 
   showMessage("Pattern Number: " + String(patternParams->patternNum));
   showMessage("Delay time: " + String(patternParams->delayTime));
   showMessage("Brightness: " + String(patternParams->brightness));
   showMessage("Leds Count: " + String(patternParams->ledsCount));
-  //showMessage("Pin: " + patternParams->pin);
 
   return patternParams;
 }
@@ -140,24 +125,18 @@ PatternParams* SetPatternAttributes(String commandString){
 // Create and return a new Pattern instance based on the given preset name
 Pattern* CreatePattern(String commandString){
   PatternParams* patternParams = SetPatternAttributes(commandString);
+  Pattern* pattern = NULL;
   switch (patternParams->patternNum){
     case 0:{
-      delete fillSolidObj;
-      showMessage("HELLO Index 0 of colours: " + String(patternParams->colours[0]));
-      showMessage("HELLO Index 1 of colours: " + String(patternParams->colours[1]));
-      showMessage("HELLO Index 2 of colours: " + String(patternParams->colours[2]));
-      fillSolidObj = new FillSolid(patternParams);   
-      return fillSolidObj;
-//      return new FillSolid(patternParams);
+      pattern = new FillSolid(patternParams);   
     }
     case 1:{
-      delete coloursFadeObj;
-      coloursFadeObj = new ColoursFade(patternParams);
-      return coloursFadeObj;
-      //return new ColoursFade(patternParams);
+      pattern = new ColoursFade(patternParams);
     }
     default:{
-      return NULL;
+      showMessage("Incorrect pattern number: " + patternParams->patternNum);
     }
   }
+  delete patternParams;
+  return pattern;
 }
