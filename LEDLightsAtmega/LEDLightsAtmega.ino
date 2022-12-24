1550d26d05d1ea89b6b02317be8b7771d2b1279b
dinus <dinus@hotmail.com>
2022-12-24 10:53:14 -0500

    Small fixes. Fix switch statement. Disable fastled interrupts.

--- LEDLightsAtmega/LEDLightsAtmega.ino
+++ LEDLightsAtmega/LEDLightsAtmega.ino
@@ -1,10 +1,11 @@
+#define FASTLED_ALLOW_INTERRUPTS 0
 #include <Arduino.h>
 #include <FastLED.h>
 #include <Pattern.h>
 #include <FillSolid.h>
 #include <ColoursFade.h>
 
-const int data_pin = 5;
+const int dataPin = 5;
 const int ledsCount = 495;
 char goodReply[] = "Atmega Ok!";
 char badReply[] = "Atmega Error";
@@ -18,15 +19,14 @@ CRGB leds[ledsCount];
 
 char oneChar;
 String packetString = "";
-
 int patternDelayBrightness [3];
-
 Pattern* currentPattern;
+PatternParams* patternParams;
 
 void setup() {
   Serial1.begin(9600);
-  FastLED.addLeds<WS2812, data_pin, RGB>(leds, ledsCount);
-  pinMode(data_pin, OUTPUT);
+  FastLED.addLeds<WS2812, dataPin, RGB>(leds, ledsCount);
+  pinMode(dataPin, OUTPUT);
 }
 
 void showMessage(String message){
@@ -58,16 +58,37 @@ void loop() {
   if (packetString != "" && fullCommandReceived){
     showMessage("Received command string: " + packetString);
     fullCommandReceived = false;
-    delete currentPattern;
-    currentPattern = CreatePattern(packetString);
+    if (patternParams != NULL) {
+      delete patternParams;
+    }
+    patternParams = SetPatternAttributes(packetString);
+    if (currentPattern != NULL) {
+      delete currentPattern;
+    }
+    currentPattern = CreatePattern(patternParams);
     packetString = "";
-    currentPattern->Start();
+    if (currentPattern != NULL) {
+      currentPattern->Start();
+    }
   } else if (currentPattern != NULL){
-    bool yeah = currentPattern->NextStep();
-    showMessage(String(yeah));
-    showMessage("Continuie executing existing pattern");
+    currentPattern->NextStep();
   } else {
     showMessage("Unexpected command received");
+  }
+}
+
+// Create and return a new Pattern instance based on the given preset name
+Pattern* CreatePattern(PatternParams* patternParams){
+  switch (patternParams->patternNum){
+    case 0:{
+      return new FillSolid(patternParams);   
+    }
+    case 1:{
+      return new ColoursFade(patternParams);
+    }
+    default:{
+      return NULL;
+    }
   }
 }
 
@@ -87,56 +108,19 @@ PatternParams* SetPatternAttributes(String commandString){
       // Cut the period, then read the number until the next period and add it to the list representing the RGB colour
       commandString = CutPeriod(commandString);
       patternParams->colours[j] = (commandString.substring(0, commandString.indexOf('.'))).toInt();
-      showMessage(String(patternParams->colours[j]));
     }
     // Cut the period once more to get rid of the blue number
     commandString = CutPeriod(commandString);
     i+=3;
   }
-  showMessage("Right before pattern num: " + commandString);
   patternParams->patternNum = (commandString.substring(1, commandString.indexOf('.')).toInt());
   commandString = CutPeriod(commandString);
-  showMessage("Right before delay time: " + commandString);
   patternParams->delayTime = (commandString.substring(1, commandString.indexOf('.')).toInt());
   commandString = CutPeriod(commandString);
-  showMessage("Right before brightness: " + commandString);
   patternParams->brightness = (commandString.substring(1, commandString.indexOf('.')).toInt());
   commandString = CutPeriod(commandString);
-
-  showMessage("Index 0 of colours: " + String(patternParams->colours[0]));
-  showMessage("Index 1 of colours: " + String(patternParams->colours[1]));
-  showMessage("Index 2 of colours: " + String(patternParams->colours[2]));
-  showMessage("Index 3 of colours: " + String(patternParams->colours[3]));
-  showMessage("Index 4 of colours: " + String(patternParams->colours[4]));
-  showMessage("Index 5 of colours: " + String(patternParams->colours[5]));
-
-  showMessage("Right before leds Count and leds: " + commandString);
   patternParams->ledsCount = ledsCount;
   patternParams->leds = leds;
 
-  showMessage("Pattern Number: " + String(patternParams->patternNum));
-  showMessage("Delay time: " + String(patternParams->delayTime));
-  showMessage("Brightness: " + String(patternParams->brightness));
-  showMessage("Leds Count: " + String(patternParams->ledsCount));
-
   return patternParams;
-}
-
-// Create and return a new Pattern instance based on the given preset name
-Pattern* CreatePattern(String commandString){
-  PatternParams* patternParams = SetPatternAttributes(commandString);
-  Pattern* pattern = NULL;
-  switch (patternParams->patternNum){
-    case 0:{
-      pattern = new FillSolid(patternParams);   
-    }
-    case 1:{
-      pattern = new ColoursFade(patternParams);
-    }
-    default:{
-      showMessage("Incorrect pattern number: " + patternParams->patternNum);
-    }
-  }
-  delete patternParams;
-  return pattern;
 }
